fe8c7dd75dfc447d1e868f94496cd8fe3c853bb9
simon245 <simonball245@gmail.com>
2015-08-28 23:52:43 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LocationFinder::Application.config.secret_key_base = '483b58b27379b6d9a84bcc1d6ae1b94e8d6fc37794e72b13f295d601b1dc357202dc1fad8179ba931e1f61d157b42a569660cb8049ed7c39d8515ea5dc95316c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LocationFinder::Application.config.session_store :cookie_store, key: '_location_finder_session'
