a18ae40a9d786ba89161d9c241327dcf680909ae
simon245 <simonball245@gmail.com>
2015-08-31 11:20:49 +0800

    before action to make sure correct and logged in user. User cannot access other users pages

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,26 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  def setup
+    @user = users(:simon)
+    @other_user = users(:jd)
+  end
+
   test "should get new" do
     get :new
     assert_response :success
   end
 
+  test "should redirect to home when not logged in" do
+    get :show, id: @user
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect when logged in as wrong user" do
+    log_in_as(@other_user)
+    get :show, id:@user
+    assert flash.empty?
+    assert_redirected_to root_url
+  end
 end
